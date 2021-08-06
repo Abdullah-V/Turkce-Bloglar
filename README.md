efd91214535100bb18fb0a2996668eff3be2a971
Abdullah <abdullahveliyev2005@gmail.com>
2021-08-06 15:13:40 +0400

    Add new blogs

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Yazılım, programlama, teknoloji ve kariyer hakkındaki türkçe bloglar.
 - [Sorun bildir](https://github.com/Abdullah-V/Turkce-Bloglar/issues/new)
 
 # Alfabe
+
 - [A](https://github.com/Abdullah-V/Turkce-Bloglar#A)
 - [B](https://github.com/Abdullah-V/Turkce-Bloglar#B)
 - [C](https://github.com/Abdullah-V/Turkce-Bloglar#C)
@@ -34,33 +35,35 @@ Yazılım, programlama, teknoloji ve kariyer hakkındaki türkçe bloglar.
 - Adem İlter - [medium.com/@ademilter](https://medium.com/@ademilter)
 - Armağan Amcalar - [arm.ag](https://arm.ag/)
 - Arman Özak - [armanozak.com](https://www.armanozak.com/)
-
 - Aykırı Yazılımcılar - [medium.com/aykiri-yazilimcilar](https://medium.com/aykiri-yazilimcilar)
 - Aykut Kardaş -[aykutkardas.medium.com](https://aykutkardas.medium.com/)
+- Ayşegül Akbaş - [medium.com/@aysegulakbas](https://medium.com/@aysegulakbas)
 - Ayyüce Kızrak - [ayyucekizrak.medium.com](https://ayyucekizrak.medium.com/)
 - Azmi Mengü - [azmimengu.medium.com](https://azmimengu.medium.com/)
 
-
 # B
+
+- Begüm Kırkgöz Demirci - [kodlayananne.medium.com](https://kodlayananne.medium.com/)
 - Bilgem Cakir - [medium.com/@bilgemcakir](https://medium.com/@bilgemcakir)
 - Bora Kaşmer - [borakasmer.com](https://www.borakasmer.com/)
 - Burak Selim Şenyurt - [burakselyum.medium.com](https://burakselyum.medium.com/)
 - Burak Selim Şenyurt - [buraksenyurt.com](https://www.buraksenyurt.com/)
 - Burcu TOPÇU - [burcutopcu96.medium.com](https://burcutopcu96.medium.com/)
 
-
 # C
+
 - Cloud and Serverless Turkey - [medium.com/cloud-turkey](https://medium.com/cloud-turkey)
 
 # D
+
 - Deniz İrgin - [denizirgin.com](https://denizirgin.com/)
 - Deniz Parlak - [den1z.sh](https://den1z.sh/)
 - Deniz TÜRKMEN - [deniz-turkmen.medium.com](https://deniz-turkmen.medium.com/)
 - Devnot - [devnot.com](https://devnot.com/)
 - Doğan Aydın - [dogan-aydin.medium.com](https://dogan-aydin.medium.com/)
 
-
 # E
+
 - Emin Fedar - [eminfedar.com](https://eminfedar.com/)
 - Emir Karşıyakalı - [medium.com/@EmirKarsiyakali](https://medium.com/@EmirKarsiyakali)
 - Emre Savcı - [mstryoda.medium.com](https://mstryoda.medium.com/)
@@ -70,87 +73,118 @@ Yazılım, programlama, teknoloji ve kariyer hakkındaki türkçe bloglar.
 - Erhan Yakut - [yakuter.com](https://www.yakuter.com/)
 - Ezran Bayantemur - [ezranbayantemur.medium.com](https://ezranbayantemur.medium.com/)
 
-
 # F
+
 - Fatih Acet - [fatihacet.com](https://fatihacet.com/)
 - Fatih Kadir Akın - [fkadev.medium.com](https://fkadev.medium.com/)
 - Furkan Gulsen - [furkangulsen.medium.com](https://furkangulsen.medium.com/)
 - Fırat Demirel - [firatdemirel.medium.com](https://firatdemirel.medium.com/)
 
-
 # G
+
 - Gökhan Gökalp - [gokhan-gokalp.com](https://www.gokhan-gokalp.com/)
 - Gökhan Şengün - [gokhansengun.com](https://gokhansengun.com/)
 - Gökhan Topçu - [medium.com/@ifndefgt](https://medium.com/@ifndefgt)
 - Gökmen Bayram - [gokmenbayram.medium.com](https://gokmenbayram.medium.com/)
 
-
 # H
+
 - Halil Özel - [halilozel1903.medium.com](https://halilozel1903.medium.com/)
 - Hakan Ertan - [seyyahil.medium.com](https://seyyahil.medium.com/)
 - Hatice Edis - [edisdev.medium.com](https://edisdev.medium.com)
 - Hüseyin Mert - [hmert.medium.com](https://hmert.medium.com/)
 
-
 # İ
+
 - İbrahim Demirli - [ibrahimdemirli.medium.com](https://ibrahimdemirli.medium.com/)
 - İrem Kömürcü - [iremkomurcu.medium.com](https://iremkomurcu.medium.com/)
 
-
 # K
+
 - Kaan Ersoy - [kaanersoy.medium.com](https://kaanersoy.medium.com/)
 - Kodcular - [medium.com/kodcular](https://medium.com/kodcular)
 - Kodluyoruz - [medium.com/@kodluyoruz](https://medium.com/@kodluyoruz)
-
+- Kodcu Herif - [kodcuherif.com](https://www.kodcuherif.com/)
 
 # M
+
 - Mertcan Gökgöz - [mertcangokgoz.com](https://mertcangokgoz.com/)
 - Mert Cobanov - [mertcobanov.medium.com](https://mertcobanov.medium.com/)
 - Mert Susur - [mertsusur.medium.com](https://mertsusur.medium.com/)
 - Merve Arslan - [arslannmrv.medium.com](https://arslannmrv.medium.com/)
 - Merve Cankiz Coruh - [medium.com/@mcankizcoruh](https://medium.com/@mcankizcoruh)
+- Merve Noyan - [merveenoyan.medium.com](https://merveenoyan.medium.com/)
 - Muhammed Hilmi Koca - [mhkoca.medium.com](https://mhkoca.medium.com/)
 - Muhammet Kara - [muhammetkara.com](https://muhammetkara.com/)
 - Mustafa Dikyar - [mustafadikyar.medium.com](https://mustafadikyar.medium.com/)
 - Mustafa Taş - [medium.com/@m2ustafa.tas](https://medium.com/@m2ustafa.tas)
 
-
 # N
+
+- Nafi Durmuş -[medium.com/@nafidurmus](https://medium.com/@nafidurmus)
 - Nesrin Asan - [nesrinasan.medium.com](https://nesrinasan.medium.com/)
 
-
 # O
+
 - Oktay ELİPEK - [medium.com/@oktayelipek](https://medium.com/@oktayelipek)
+- Onur Dayıbaşı - [odayibasi.medium.com](https://odayibasi.medium.com/)
 
 # Ö
+
 - Önceki Yazılımcı - [oncekiyazilimci.medium.com](https://oncekiyazilimci.medium.com/)
 - Özgür Adem Işıklı - [ozguradem.net](https://ozguradem.net/blog/)
 
-
 # P
+
 - Producter - [blog.producter.co](https://blog.producter.co/)
 - Prototurk - [prototurk.com/makaleler](https://prototurk.com/makaleler)
 
-
 # R
+
 - Recep Fırıncıoğlu - [recepfirincioglu.medium.com](https://recepfirincioglu.medium.com/)
 - Recep Orhan - [orhanrecep90.medium.com](https://orhanrecep90.medium.com/)
 
-
 # S
+
 - Safa Orhan -[safaorhan.medium.com](https://safaorhan.medium.com/)
 - Samet Özkale - [https://sametozkale.medium.com/](https://sametozkale.medium.com/)
 - Selçuk Cihan - [blog.selcukcihan.com](https://blog.selcukcihan.com)
 - Selçuk Ermaya - [selcukermaya.com/tr](https://www.selcukermaya.com/tr/)
 - SwordSec - [swordsec.com/tr/category/blog-tr/](https://swordsec.com/tr/category/blog-tr/)
 
-
 # T
+
 - TalentGrid - [talentgrid.io/blog](https://talentgrid.io/blog/)
 - Tarik Guney - [atarikguney.medium.com](https://atarikguney.medium.com/)
 - Tayfun Erbilen - [erbilen.net](https://erbilen.net)
-
+- Tuğberk Can Özen - [tugberkcanozen.medium.com](https://tugberkcanozen.medium.com/)
 
 # U
+
 - Uğur Arıcı - [ugurarici.com](https://ugurarici.com/)
+- Umut Acerbastimur - [umutacerbastimur.medium.com](https://umutacerbastimur.medium.com/)
 - Umut Gökbayrak - [umutgokbayrak.com](https://umutgokbayrak.com)
+
+# Lisans (MIT)
+
+MIT License
+
+Copyright (c) 2021 Abdullah Veliyev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- website/data.js
+++ website/data.js
@@ -1,172 +1,186 @@
 export default [
   {
-    letter: "a",
+    letter: 'a',
     blogs: [
-      { name: "Adem İlter", link: "https://medium.com/@ademilter" },
-      { name: "Armağan Amcalar", link: "https://arm.ag" },
-      { name: "Arman Özak", link: "https://www.armanozak.com" },
+      { name: 'Adem İlter', link: 'https://medium.com/@ademilter' },
+      { name: 'Armağan Amcalar', link: 'https://arm.ag' },
+      { name: 'Arman Özak', link: 'https://www.armanozak.com' },
       {
-        name: "Aykırı Yazılımcılar",
-        link: "https://medium.com/aykiri-yazilimcilar",
+        name: 'Aykırı Yazılımcılar',
+        link: 'https://medium.com/aykiri-yazilimcilar'
       },
-      { name: "Aykut Kardaş", link: "https://aykutkardas.medium.com" },
-      { name: "Ayyüce Kızrak", link: "https://ayyucekizrak.medium.com" },
-      { name: "Azmi Mengü", link: "https://azmimengu.medium.com" },
-    ],
+      { name: 'Aykut Kardaş', link: 'https://aykutkardas.medium.com' },
+      { name: 'Ayşegül Akbaş', link: 'https://medium.com/@aysegulakbas' },
+      { name: 'Ayyüce Kızrak', link: 'https://ayyucekizrak.medium.com' },
+      { name: 'Azmi Mengü', link: 'https://azmimengu.medium.com' }
+    ]
   },
   {
-    letter: "b",
-    blogs: [
-      { name: "Bilgem Cakir", link: "https://medium.com/@bilgemcakir" },
-      { name: "Bora Kaşmer", link: "https://www.borakasmer.com" },
-      { name: "Burak Selim Şenyurt", link: "https://burakselyum.medium.com" },
-      { name: "Burak Selim Şenyurt", link: "https://buraksenyurt.com" },
-      { name: "Burcu TOPÇU", link: "https://burcutopcu96.medium.com" },
-    ],
-  },
-  {
-    letter: "c",
+    letter: 'b',
     blogs: [
       {
-        name: "Cloud and Serverless Turkey",
-        link: "https://medium.com/cloud-turkey",
+        name: 'Begüm Kırkgöz Demirci',
+        link: 'https://kodlayananne.medium.com'
       },
-    ],
+      { name: 'Bilgem Cakir', link: 'https://medium.com/@bilgemcakir' },
+      { name: 'Bora Kaşmer', link: 'https://www.borakasmer.com' },
+      { name: 'Burak Selim Şenyurt', link: 'https://burakselyum.medium.com' },
+      { name: 'Burak Selim Şenyurt', link: 'https://buraksenyurt.com' },
+      { name: 'Burcu TOPÇU', link: 'https://burcutopcu96.medium.com' }
+    ]
   },
   {
-    letter: "d",
-    blogs: [
-      { name: "Deniz İrgin", link: "https://denizirgin.com" },
-      { name: "Deniz Parlak", link: "https://den1z.sh" },
-      { name: "Deniz TÜRKMEN", link: "https://deniz-turkmen.medium.com" },
-      { name: "Devnot", link: "https://devnot.com" },
-      { name: "Doğan Aydın", link: "https://dogan-aydin.medium.com" },
-    ],
-  },
-  {
-    letter: "e",
-    blogs: [
-      { name: "Emin Fedar", link: "https://eminfedar.com" },
-      { name: "Emir Karşıyakalı", link: "https://medium.com/@EmirKarsiyakali" },
-      { name: "Emre Savcı", link: "https://mstryoda.medium.com" },
-      { name: "Emre MERT", link: "https://emremert117.medium.com" },
-      { name: "Enes Öztürk", link: "https://ozturkenes.com/blog" },
-      { name: "Eser Özvataf", link: "https://eser.dev/blog" },
-      { name: "Erhan Yakut", link: "https://www.yakuter.com" },
-      { name: "Ezran Bayantemur", link: "https://ezranbayantemur.medium.com" },
-    ],
-  },
-  {
-    letter: "f",
-    blogs: [
-      { name: "Fatih Acet", link: "https://fatihacet.com" },
-      { name: "Fatih Kadir Akın", link: "https://fkadev.medium.com" },
-      { name: "Furkan Gulsen", link: "https://furkangulsen.medium.com" },
-      { name: "Fırat Demirel", link: "https://firatdemirel.medium.com" },
-    ],
-  },
-  {
-    letter: "g",
-    blogs: [
-      { name: "Gökhan Gökalp", link: "https://www.gokhan-gokalp.com" },
-      { name: "Gökhan Şengün", link: "https://gokhansengun.com" },
-      { name: "Gökhan Topçu", link: "https://medium.com/@ifndefgt" },
-      { name: "Gökmen Bayram", link: "https://gokmenbayram.medium.com" },
-    ],
-  },
-  {
-    letter: "h",
-    blogs: [
-      { name: "Halil Özel", link: "https://halilozel1903.medium.com" },
-      { name: "Hakan Ertan", link: "https://seyyahil.medium.com" },
-      { name: "Hatice Edis", link: "https://edisdev.medium.com" },
-      { name: "Hüseyin Mert", link: "https://hmert.medium.com" },
-    ],
-  },
-  {
-    letter: "i",
-    blogs: [
-      { name: "İbrahim Demirli", link: "https://ibrahimdemirli.medium.com" },
-      { name: "İrem Kömürcü", link: "https://iremkomurcu.medium.com" },
-    ],
-  },
-  {
-    letter: "k",
-    blogs: [
-      { name: "Kaan Ersoy", link: "https://kaanersoy.medium.com" },
-      { name: "Kodcular", link: "https://medium.com/kodcular" },
-      { name: "Kodluyoruz", link: "https://medium.com/@kodluyoruz" },
-    ],
-  },
-  {
-    letter: "m",
-    blogs: [
-      { name: "Mertcan Gökgöz", link: "https://mertcangokgoz.com" },
-      { name: "Mert Cobanov", link: "https://mertcobanov.medium.com" },
-      { name: "Mert Susur", link: "https://mertsusur.medium.com" },
-      { name: "Merve Arslan", link: "https://arslannmrv.medium.com" },
-      { name: "Merve Cankiz Coruh", link: "https://medium.com/@mcankizcoruh" },
-      { name: "Muhammed Hilmi Koca", link: "https://mhkoca.medium.com" },
-      { name: "Muhammet Kara", link: "https://muhammetkara.com" },
-      { name: "Mustafa Dikyar", link: "https://mustafadikyar.medium.com" },
-      { name: "Mustafa Taş", link: "https://medium.com/@m2ustafa.tas" },
-    ],
-  },
-  {
-    letter: "n",
-    blogs: [{ name: "Nesrin Asan", link: "https://nesrinasan.medium.com" }],
-  },
-  {
-    letter: "o",
-    blogs: [{ name: "Oktay ELİPEK", link: "https://medium.com/@oktayelipek" }],
-  },
-  {
-    letter: "ö",
-    blogs: [
-      { name: "Önceki Yazılımcı", link: "https://oncekiyazilimci.medium.com" },
-      { name: "Özgür Adem Işıklı", link: "https://ozguradem.net/blog" },
-    ],
-  },
-  {
-    letter: "p",
-    blogs: [
-      { name: "Producter", link: "https://blog.producter.co" },
-      { name: "Prototurk", link: "https://prototurk.com/makaleler" },
-    ],
-  },
-  {
-    letter: "r",
+    letter: 'c',
     blogs: [
       {
-        name: "Recep Fırıncıoğlu",
-        link: "https://recepfirincioglu.medium.com",
+        name: 'Cloud and Serverless Turkey',
+        link: 'https://medium.com/cloud-turkey'
+      }
+    ]
+  },
+  {
+    letter: 'd',
+    blogs: [
+      { name: 'Deniz İrgin', link: 'https://denizirgin.com' },
+      { name: 'Deniz Parlak', link: 'https://den1z.sh' },
+      { name: 'Deniz TÜRKMEN', link: 'https://deniz-turkmen.medium.com' },
+      { name: 'Devnot', link: 'https://devnot.com' },
+      { name: 'Doğan Aydın', link: 'https://dogan-aydin.medium.com' }
+    ]
+  },
+  {
+    letter: 'e',
+    blogs: [
+      { name: 'Emin Fedar', link: 'https://eminfedar.com' },
+      { name: 'Emir Karşıyakalı', link: 'https://medium.com/@EmirKarsiyakali' },
+      { name: 'Emre Savcı', link: 'https://mstryoda.medium.com' },
+      { name: 'Emre MERT', link: 'https://emremert117.medium.com' },
+      { name: 'Enes Öztürk', link: 'https://ozturkenes.com/blog' },
+      { name: 'Eser Özvataf', link: 'https://eser.dev/blog' },
+      { name: 'Erhan Yakut', link: 'https://www.yakuter.com' },
+      { name: 'Ezran Bayantemur', link: 'https://ezranbayantemur.medium.com' }
+    ]
+  },
+  {
+    letter: 'f',
+    blogs: [
+      { name: 'Fatih Acet', link: 'https://fatihacet.com' },
+      { name: 'Fatih Kadir Akın', link: 'https://fkadev.medium.com' },
+      { name: 'Furkan Gulsen', link: 'https://furkangulsen.medium.com' },
+      { name: 'Fırat Demirel', link: 'https://firatdemirel.medium.com' }
+    ]
+  },
+  {
+    letter: 'g',
+    blogs: [
+      { name: 'Gökhan Gökalp', link: 'https://www.gokhan-gokalp.com' },
+      { name: 'Gökhan Şengün', link: 'https://gokhansengun.com' },
+      { name: 'Gökhan Topçu', link: 'https://medium.com/@ifndefgt' },
+      { name: 'Gökmen Bayram', link: 'https://gokmenbayram.medium.com' }
+    ]
+  },
+  {
+    letter: 'h',
+    blogs: [
+      { name: 'Halil Özel', link: 'https://halilozel1903.medium.com' },
+      { name: 'Hakan Ertan', link: 'https://seyyahil.medium.com' },
+      { name: 'Hatice Edis', link: 'https://edisdev.medium.com' },
+      { name: 'Hüseyin Mert', link: 'https://hmert.medium.com' }
+    ]
+  },
+  {
+    letter: 'i',
+    blogs: [
+      { name: 'İbrahim Demirli', link: 'https://ibrahimdemirli.medium.com' },
+      { name: 'İrem Kömürcü', link: 'https://iremkomurcu.medium.com' }
+    ]
+  },
+  {
+    letter: 'k',
+    blogs: [
+      { name: 'Kaan Ersoy', link: 'https://kaanersoy.medium.com' },
+      { name: 'Kodcular', link: 'https://medium.com/kodcular' },
+      { name: 'Kodluyoruz', link: 'https://medium.com/@kodluyoruz' },
+      { name: 'Kodcu Herif', link: 'https://www.kodcuherif.com' }
+    ]
+  },
+  {
+    letter: 'm',
+    blogs: [
+      { name: 'Mertcan Gökgöz', link: 'https://mertcangokgoz.com' },
+      { name: 'Mert Cobanov', link: 'https://mertcobanov.medium.com' },
+      { name: 'Mert Susur', link: 'https://mertsusur.medium.com' },
+      { name: 'Merve Arslan', link: 'https://arslannmrv.medium.com' },
+      { name: 'Merve Cankiz Coruh', link: 'https://medium.com/@mcankizcoruh' },
+      { name: 'Merve Noyan', link: 'https://merveenoyan.medium.com' },
+      { name: 'Muhammed Hilmi Koca', link: 'https://mhkoca.medium.com' },
+      { name: 'Muhammet Kara', link: 'https://muhammetkara.com' },
+      { name: 'Mustafa Dikyar', link: 'https://mustafadikyar.medium.com' },
+      { name: 'Mustafa Taş', link: 'https://medium.com/@m2ustafa.tas' }
+    ]
+  },
+  {
+    letter: 'n',
+    blogs: [{ name: 'Nafi Durmuş', link: 'https://medium.com/@nafidurmus' }],
+    blogs: [{ name: 'Nesrin Asan', link: 'https://nesrinasan.medium.com' }]
+  },
+  {
+    letter: 'o',
+    blogs: [{ name: 'Oktay ELİPEK', link: 'https://medium.com/@oktayelipek' }],
+    blogs: [{ name: 'Onur Dayıbaşı', link: 'https://odayibasi.medium.com' }]
+  },
+  {
+    letter: 'ö',
+    blogs: [
+      { name: 'Önceki Yazılımcı', link: 'https://oncekiyazilimci.medium.com' },
+      { name: 'Özgür Adem Işıklı', link: 'https://ozguradem.net/blog' }
+    ]
+  },
+  {
+    letter: 'p',
+    blogs: [
+      { name: 'Producter', link: 'https://blog.producter.co' },
+      { name: 'Prototurk', link: 'https://prototurk.com/makaleler' }
+    ]
+  },
+  {
+    letter: 'r',
+    blogs: [
+      {
+        name: 'Recep Fırıncıoğlu',
+        link: 'https://recepfirincioglu.medium.com'
       },
-      { name: "Recep Orhan", link: "https://orhanrecep90.medium.com" },
-    ],
+      { name: 'Recep Orhan', link: 'https://orhanrecep90.medium.com' }
+    ]
   },
   {
-    letter: "s",
+    letter: 's',
     blogs: [
-      { name: "Safa Orhan", link: "https://safaorhan.medium.com" },
-      { name: "Samet Özkale", link: "https://sametozkale.medium.com" },
-      { name: "Selçuk Cihan", link: "https://blog.selcukcihan.com" },
-      { name: "Selçuk Ermaya", link: "https://www.selcukermaya.com/tr" },
-      { name: "SwordSec", link: "https://swordsec.com/tr/category/blog-tr" },
-    ],
+      { name: 'Safa Orhan', link: 'https://safaorhan.medium.com' },
+      { name: 'Samet Özkale', link: 'https://sametozkale.medium.com' },
+      { name: 'Selçuk Cihan', link: 'https://blog.selcukcihan.com' },
+      { name: 'Selçuk Ermaya', link: 'https://www.selcukermaya.com/tr' },
+      { name: 'SwordSec', link: 'https://swordsec.com/tr/category/blog-tr' }
+    ]
   },
   {
-    letter: "t",
+    letter: 't',
     blogs: [
-      { name: "TalentGrid", link: "https://talentgrid.io/blog" },
-      { name: "Tarik Guney", link: "https://atarikguney.medium.com" },
-      { name: "Tayfun Erbilen", link: "https://erbilen.net" },
-    ],
+      { name: 'TalentGrid', link: 'https://talentgrid.io/blog' },
+      { name: 'Tarik Guney', link: 'https://atarikguney.medium.com' },
+      { name: 'Tayfun Erbilen', link: 'https://erbilen.net' },
+      { name: 'Tuğberk Can Özen', link: 'https://tugberkcanozen.medium.com' }
+    ]
   },
   {
-    letter: "u",
+    letter: 'u',
     blogs: [
-      { name: "Uğur Arıcı", link: "https://ugurarici.com" },
-      { name: "Umut Gökbayrak", link: "https://umutgokbayrak.com" },
-    ],
-  },
-];
+      { name: 'Uğur Arıcı', link: 'https://ugurarici.com' },
+      {
+        name: 'Umut Acerbastimur',
+        link: 'https://umutacerbastimur.medium.com'
+      },
+      { name: 'Umut Gökbayrak', link: 'https://umutgokbayrak.com' }
+    ]
+  }
+]
